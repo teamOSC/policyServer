9db6b9c0229176e60f7c6538f4ab17a3df786d60
Shubham Dokania <skd.1810@gmail.com>
2015-04-18 18:05:02 +0530

    Here lies the treause of the source code

--- app/views.py
+++ app/views.py
@@ -1,2 +1,26 @@
 from flask import Flask,render_template, flash, redirect, \
     make_response,request, url_for , session, g, jsonify
+import json
+from tweets import fetchTweet
+from app import app
+
+
+@app.route('/api/bar', methods=['GET', 'POST'])
+def main():
+    try:
+        barcode = request.args.post('barcode')
+    except:
+        barcode = request.args.get('barcode')
+    return json.dumps(str(barcode))
+
+
+@app.route('/admin')
+def admin():
+    userTweets = fetchTweet('sauravtom')
+    print type(userTweets), userTweets
+    return render_template('admin.html', tweet=userTweets)
+
+
+@app.route('/admin/warnLocal')
+def localWarn():
+    pass
